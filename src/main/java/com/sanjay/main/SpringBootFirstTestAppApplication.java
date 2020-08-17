4d9202f942ad70630c2051b5a13573abf2a4659a
Sanjay <Sanjay@DESKTOP-ANINC4H>
2020-08-17 15:27:11 +0530

    WH-8520- FirstTime Update

---
+++ src/main/java/com/sanjay/main/SpringBootFirstTestAppApplication.java
@@ -0,0 +1,13 @@
+package com.sanjay.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFirstTestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFirstTestAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sanjay/main/SpringBootFirstTestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanjay.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFirstTestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
